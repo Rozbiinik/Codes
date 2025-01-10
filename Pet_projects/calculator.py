e96e13934a62b13d41b300ac37e20b6a7d1dd5a5
Airat <mavliaveev.airat@yandex.ru>
2025-01-10 11:55:16 +0300

    5

--- Pet_projects/calculator.py
+++ Pet_projects/calculator.py
@@ -5,6 +5,9 @@ def calculator(num, base):
         remains = num % base
         num = quotient
         total += str(remains)
+    if num == 1:
+        total += '1'
+    
     return total [::-1]
 
 calculator(513, 2)
